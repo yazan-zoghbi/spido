1c3522d9538c9c7ee5780d0c8c720f6b1946692a
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2022-04-08 07:43:47 +0300

    [EDIT]: changes log updated!

---
+++ changes.md
@@ -0,0 +1,11 @@
+## Features Added
+
+- cli commands integration
+- new argument `-u` for url
+- new argument `-f` to fetch
+- new argument `-c` to crawl
+
+## Fixed bugs
+
+- crawl internal links bug
+- fetch bug
--- test/crawl.test.js
+++ test/crawl.test.js
@@ -1,8 +1,8 @@
 const Spido = require("../lib/crawler");
 
-const url = "https://www.scandinaviatech.com";
+const url = "https://www.google.com";
 const c = new Spido(url);
 
-const response = c.fetch(url);
+const response = c.crawl(url);
 
 console.log(response);
